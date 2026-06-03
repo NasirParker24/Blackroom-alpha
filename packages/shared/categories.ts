e68abd9eba80b66c9545ca6568b05f2f2faed4c5
NasirParker24 <118922864+NasirParker24@users.noreply.github.com>
2026-06-03 13:07:13 -0400

    Add category library

---
+++ packages/shared/categories.ts
@@ -0,0 +1,3 @@
+export const CATEGORIES = [
+'Politics','Economics','Technology','Culture','Music','Fashion','Sports','Religion','Race','Gender','Work','Education','Luxury','Black Culture','Local Culture','Relationships','Health & Wellness','Creator Economy','Entrepreneurship','Media'
+] as const;
