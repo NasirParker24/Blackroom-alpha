15ef7bdef11bd2ad33c14f9e96d212177e381275
NasirParker24 <118922864+NasirParker24@users.noreply.github.com>
2026-06-03 13:10:56 -0400

    Add narrative library

---
+++ packages/shared/narratives.ts
@@ -0,0 +1,12 @@
+export const NARRATIVES = [
+  'Rebellion',
+  'Restoration',
+  'Liberation',
+  'Reinvention',
+  'Redemption',
+  'Survival',
+  'Belonging',
+  'Ascension',
+  'Preservation',
+  'Self-Determination',
+] as const;
