bac1a9c9dfb13dd806d7d5630acddc7c9b2e9812
NasirParker24 <118922864+NasirParker24@users.noreply.github.com>
2026-06-03 13:11:21 -0400

    Add ideology library

---
+++ packages/shared/ideologies.ts
@@ -0,0 +1,3 @@
+export const IDEOLOGIES = [
+'Soft Life','Quiet Luxury','Digital Sovereignty','Localism','Creator Independence','Black Ownership','Afrofuturism','Wellness Capitalism','Neo-Traditionalism','Cultural Ownership'
+] as const;
