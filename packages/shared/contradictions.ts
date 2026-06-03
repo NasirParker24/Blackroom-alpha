5c0f481699c13f2e26cb91bfe838b911a936c582
NasirParker24 <118922864+NasirParker24@users.noreply.github.com>
2026-06-03 13:07:48 -0400

    Add contradiction library

---
+++ packages/shared/contradictions.ts
@@ -0,0 +1,3 @@
+export const CONTRADICTIONS=[
+'Achievement vs Wellbeing','Freedom vs Security','Status vs Authenticity','Community vs Individualism','Tradition vs Progress','Institutional Trust vs Personal Experience','Technology vs Humanity','Ownership vs Access','Globalization vs Local Identity','Convenience vs Meaning'
+];
