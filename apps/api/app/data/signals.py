b753bf9d9e1549075cd438418171ef18099a5faf
NasirParker24 <118922864+NasirParker24@users.noreply.github.com>
2026-06-03 13:19:32 -0400

    Add seeded signal data

---
+++ apps/api/app/data/signals.py
@@ -0,0 +1,24 @@
+SIGNALS = [
+{
+ 'signal_id':'BR-2026-003',
+ 'title':'AI Job Displacement',
+ 'category':'Technology',
+ 'heat_score':9,
+ 'velocity_score':9,
+ 'intensity_score':8,
+ 'longevity_score':9,
+ 'commercial_score':8,
+ 'cultural_score':9
+},
+{
+ 'signal_id':'BR-2026-022',
+ 'title':'Black Luxury',
+ 'category':'Luxury',
+ 'heat_score':8,
+ 'velocity_score':7,
+ 'intensity_score':8,
+ 'longevity_score':8,
+ 'commercial_score':9,
+ 'cultural_score':9
+}
+]
